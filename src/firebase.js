e61623727c92e61f2b4cb12c29a58a837216c7c5
milan8411 <milangadhvi8411@gmail.com>
2023-04-21 20:39:31 +0530

    add Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBAbZrps_8douaRiT8-zcuaz-e0HMMBPPs",
-  authDomain: "linkedin-clone-8ca0e.firebaseapp.com",
-  projectId: "linkedin-clone-8ca0e",
-  storageBucket: "linkedin-clone-8ca0e.appspot.com",
-  messagingSenderId: "610605743671",
-  appId: "1:610605743671:web:865936441b6db8d7d3c9b2",
+  apiKey: "AIzaSyAsoO0PPcxnq_NL6jd_GHA5zzLKsMr8MxY",
+  authDomain: "linkedin-clone-yt-1237c.firebaseapp.com",
+  projectId: "linkedin-clone-yt-1237c",
+  storageBucket: "linkedin-clone-yt-1237c.appspot.com",
+  messagingSenderId: "406372046563",
+  appId: "1:406372046563:web:3088b2302a8129c14197d6",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
